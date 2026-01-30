57c3ba969fc5240c213bbc28441140d815e4b098
blankitolv <lucasvega2@gmail.com>
2026-01-30 02:13:31 -0300

    delete comment

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -6,18 +6,17 @@ export default (io) => {
 
     socket.on("ClientNeedProducts", async (data) => {
       console.log("(ClientNeedProducts) Mensaje recibido:", data);
-      // Assuming getAllProducts can return all products without specific query params for this use case
-      const productsResponse = await ProductService.getAllProducts({}); 
+      const productsResponse = await ProductService.getAllProducts({});
       socket.emit("serverSendProducts", productsResponse.docs);
     });
 
     socket.on("del_product", (data) => {
-      console.log("SE EMITE DEL_ ", data)
+      console.log("SE EMITE DEL_ ", data);
       io.emit("del_product", data);
     });
-    
+
     socket.on("new_product", (product) => {
-      console.log("producto creado: ",product)
+      console.log("producto creado: ", product);
       io.emit("new_product", product);
     });
 
