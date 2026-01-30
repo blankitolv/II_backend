031ef5e2d24aec57d2144fe99f8cbe21fe5c9c1f
blankitolv <lucasvega2@gmail.com>
2026-01-30 02:07:29 -0300

    add comment on config db

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import { consoleColors } from "../utils/utils.utils.js";
 
+// conexión a la base de datos MongoDB
 export const connectMongo = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URL, {
